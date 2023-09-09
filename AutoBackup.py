8d1888bb42eda9e9831edf4029a6bf145b961b81
rgoogoonani <68652911+rgoogoonani@users.noreply.github.com>
2023-09-09 13:31:44 +0330

    Update AutoBackup.py

--- AutoBackup.py
+++ AutoBackup.py
@@ -12,7 +12,7 @@ import urllib3
 urllib3.disable_warnings()
 
 lines = ""
-with open("/NginxBackup/config.txt","r+") as f :
+with open("/MssqlBackup/config.txt","r+") as f :
     lines=f.read().split("\n")
     lines=[i.replace("\r","") for i in lines]
 chid=lines[0]
@@ -31,7 +31,7 @@ def upload():
     archived = shutil.make_archive('/MsSql', 'zip', FileAddres)
 
     if os.path.exists('/MsSql.zip'):
-        requests.get(f"https://tapi.bale.ai/bot{token}/sendDocument?chat_id={chid}&caption=SQL Server Name : {Name}",files={'document': (FileName, open("/MsSql.zip", 'rb'))})
+        requests.get(f"https://api.telegram.org/bot{token}/sendDocument?chat_id={chid}&caption=SQL Server Name : {Name}",files={'document': (FileName, open("/MsSql.zip", 'rb'))})
         print("uploaded")
     else: 
         print("ZIP file not created")
